af50d957da3258e501d6081d60c26e1e6cde9f26
z <z>
2021-03-05 16:54:45 +0200

    test

---
+++ components/AppBar.vue
@@ -0,0 +1,29 @@
+<template>
+
+    <v-app-bar fixed app>
+      <v-app-bar-nav-icon @click.stop="changeDrawer"/>
+      <v-toolbar-title>{{ title }}</v-toolbar-title>
+    </v-app-bar>
+
+</template>
+
+<script>
+export default {
+
+  name: 'AppBar',
+
+  props: {
+    title: {
+      type: String,
+      required: false,
+    },
+  },
+
+  methods: {
+    changeDrawer() {
+      this.$emit('drawer');
+    },
+  },
+
+}
+</script>
---
+++ components/AppNav.vue
@@ -0,0 +1,17 @@
+<template>
+
+  <v-navigation-drawer :value="isActive" app fixed temporary/>
+
+</template>
+
+<script>
+export default {
+
+  name: 'AppNav',
+
+  props: [
+    'isActive',
+  ],
+
+}
+</script>
